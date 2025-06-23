4aee222913a8fb05729ff5a85704e7531150bdfa
Danyssa B. Tamayo <120694207+tamayodb@users.noreply.github.com>
2025-06-23 15:13:09 +0800

    django models and tables

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class List(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
