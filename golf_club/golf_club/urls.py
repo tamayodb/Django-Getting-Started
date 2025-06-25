e3b2a01363099bd67675879d62668a52849dfc96
Danyssa B. Tamayo <120694207+tamayodb@users.noreply.github.com>
2025-06-25 14:15:06 +0800

    updated routing

--- golf_club/golf_club/urls.py
+++ golf_club/golf_club/urls.py
@@ -5,7 +5,7 @@ from members import views
 
 urlpatterns = [
     path('', lambda request: redirect('main/', permanent=False)),
-    path('main/', views.main, name='main'),
+    path('main/', views.main, name='main'), 
     path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
